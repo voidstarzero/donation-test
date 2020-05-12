7f9a51ecff029d3bed578b11a5695893ca228748
James Arcus <jimbo@ucc.asn.au>
2020-05-12 21:35:37 +0800

    Make balances available through admin panel

--- cvonline/admin.py
+++ cvonline/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from . import models
 
 # Model registrations for admin panel
-admin.site.register(models.Attendee)
 admin.site.register(models.Club)
+admin.site.register(models.Balance)
+admin.site.register(models.Attendee)
 admin.site.register(models.Event)
 admin.site.register(models.Donation)
