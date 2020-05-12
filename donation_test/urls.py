733615e9451e9ed7d102fe0258ffa7bbde902ae2
James Arcus <jimbo@ucc.asn.au>
2020-05-12 16:02:54 +0800

    Add index page

--- donation_test/urls.py
+++ donation_test/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from donation_test import views
+
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('admin/', admin.site.urls), # leave to allow access to prebuilt admin site
 ]
