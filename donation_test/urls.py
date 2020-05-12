41cd8369ff19d3b5780873857e542049a1c69d1a
James Arcus <jimbo@ucc.asn.au>
2020-05-12 21:42:00 +0800

    Move views to cvonline module

--- donation_test/urls.py
+++ donation_test/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from donation_test import views
+from cvonline import views
 
 from django.contrib import admin
 from django.urls import path
