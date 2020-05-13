cf5ac9659cac60d74a81075cd14edc563c1c1514
James Arcus <jimbo@ucc.asn.au>
2020-05-13 13:01:22 +0800

    Actually write payments to the user's account

--- squarepay/views.py
+++ squarepay/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
 from django.shortcuts import render
+from django.db import transaction
 
 from square.client import Client
 from square.configuration import Configuration
@@ -9,6 +10,7 @@ import requests
 import uuid
 
 from donation_test import settings
+from .models import CardPayment
 
 client = Client(
     access_token=settings.SQUARE_ACCESS_TOKEN,
@@ -74,6 +76,22 @@ def process_payment(request):
         result = requests.post(endpoint, headers=headers, json=body)
 
         if result.status_code == 200:
+            # actually commit to payment, now
+            with transaction.atomic():
+                # credit the user's account
+                balance_obj = request.user.attendee_info.balance
+                balance_obj.balance += amount
+                balance_obj.cumulative += amount
+                balance_obj.save()
+
+                # and record the transaction
+                pay_record = CardPayment(
+                    amount=amount,
+                    idempotency_key=idempotency_key,
+                    attendee_payer=request.user.attendee_info,
+                )
+                pay_record.save()
+
             return JsonResponse({
                 'title': 'Payment Successful',
                 'result': result.json(),
