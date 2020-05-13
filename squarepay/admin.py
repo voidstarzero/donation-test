1dae060225531b8fa33a896e8da5878131e32ee6
James Arcus <jimbo@ucc.asn.au>
2020-05-13 12:18:41 +0800

    Remove some unnecessary(?) overhead

--- squarepay/admin.py
+++ squarepay/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import CardPayment
 
 class CardPaymentAdmin(admin.ModelAdmin):
-    list_display = ['amount', 'date_created', 'is_paid']
+    list_display = ['amount', 'date_paid']
     readonly_fields = ['idempotency_key']
 
 admin.site.register(CardPayment, CardPaymentAdmin)
--- squarepay/models.py
+++ squarepay/models.py
@@ -10,11 +10,4 @@ class CardPayment(models.Model):
     idempotency_key = models.CharField(max_length=64, default=uuid.uuid1) # Square API idempotency key
     attendee_payer  = models.ForeignKey(Attendee, related_name='payments',
                                                   on_delete=models.SET_NULL, null=True)
-    is_paid         = models.BooleanField(blank=True, default=False)
-    date_created    = models.DateTimeField(auto_now_add=True)
-    date_paid       = models.DateTimeField(null=True, blank=True)
-
-    def set_paid(self):
-        self.is_paid = True
-        self.date_paid = datetime.now()
-        self.save()
+    date_paid       = models.DateTimeField(auto_now_add=True)
